485474ca939e43f69cd3923af4b35a4ad3819151
rutveekloopbots <rutveek.loopbots@gmail.com>
2023-02-17 12:57:16 +0530

    Initial Commit

---
+++ ViewVideoPlayer/New Group/ViewVideoPlayer.swift
@@ -0,0 +1,14 @@
+//
+//  ViewVideoPlayer.swift
+//  ViewVideoPlayer
+//
+//  Created by Rutveek on 17/02/23.
+//
+
+import UIKit
+
+class ViewVideoPlayer: UIView {
+
+  
+
+}
---
+++ ViewVideoPlayer/ViewVideoPlayer.h
@@ -0,0 +1,18 @@
+//
+//  ViewVideoPlayer.h
+//  ViewVideoPlayer
+//
+//  Created by Rutveek on 17/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewVideoPlayer.
+FOUNDATION_EXPORT double ViewVideoPlayerVersionNumber;
+
+//! Project version string for ViewVideoPlayer.
+FOUNDATION_EXPORT const unsigned char ViewVideoPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewVideoPlayer/PublicHeader.h>
+
+
